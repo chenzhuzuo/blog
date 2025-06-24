2b16a56a69c4744a1b8a9bb0121bce079c7f71a0
chenzhuzuo <1015830383@qq.com>
2025-06-24 10:25:04 +0800

    Update landingPage.js

--- landingPage.js
+++ landingPage.js
@@ -55,4 +55,12 @@
                 });
             });
         });
+// FAQ交互
+const faqItems = document.querySelectorAll('.faq-item');
+faqItems.forEach(item => {
+    const question = item.querySelector('.faq-question');
+    question.addEventListener('click', () => {
+        item.classList.toggle('active');
+    });
+});
    
