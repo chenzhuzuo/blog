f815f9f60485606bbf2ff581cea373781c199b4d
chenzhuzuo <1015830383@qq.com>
2025-06-24 10:51:14 +0800

    Update landingPage.js
    更新faq交互

--- landingPage.js
+++ landingPage.js
@@ -41,7 +41,7 @@
                 }
             });
             // FAQ交互
-const faqItems = document.querySelectorAll('.faq-item');
+ const faqItems = document.querySelectorAll('.faq-item');
 faqItems.forEach(item => {
     const question = item.querySelector('.faq-question');
     question.addEventListener('click', () => {
